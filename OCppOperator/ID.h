07a1b39a5574be50b19fe48e58a223c8eea8e9bf
oden <oden@jitakukeibi.in>
2013-03-08 10:28:02 +0900

    ID.hとID.mmを追加 refs #1

---
+++ OCppOperator/ID.h
@@ -0,0 +1,13 @@
+//
+//  ID.h
+//  OCppOperator
+//
+//  Created by oden on 13/03/08.
+//  Copyright (c) 2013年 JITAKUKEIBI INC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ID : NSObject
+
+@end
---
+++ OCppOperator/ID.mm
@@ -0,0 +1,13 @@
+//
+//  ID.m
+//  OCppOperator
+//
+//  Created by oden on 13/03/08.
+//  Copyright (c) 2013年 JITAKUKEIBI INC. All rights reserved.
+//
+
+#import "ID.h"
+
+@implementation ID
+
+@end
