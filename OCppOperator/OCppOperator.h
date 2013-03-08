72326ce1b15d0a87849247fd81c340b2aabdca65
oden <oden@jitakukeibi.in>
2013-03-08 10:26:53 +0900

    OCppOperator.h追加 refs #1

---
+++ OCppOperator/OCppOperator.h
@@ -0,0 +1,14 @@
+//
+//  OCppOperator.h
+//  OCppOperator
+//
+//  Created by oden on 13/03/08.
+//  Copyright (c) 2013年 JITAKUKEIBI INC. All rights reserved.
+//
+
+#ifndef OCppOperator_OCppOperator_h
+#define OCppOperator_OCppOperator_h
+
+
+
+#endif
