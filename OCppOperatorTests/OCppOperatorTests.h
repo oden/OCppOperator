cd8d6725230b6b2bb4632cf74d82775926b2ef3e
oden <oden@jitakukeibi.in>
2013-03-08 09:41:10 +0900

    Initial Commit

---
+++ OCppOperatorTests/OCppOperatorTests.h
@@ -0,0 +1,13 @@
+//
+//  OCppOperatorTests.h
+//  OCppOperatorTests
+//
+//  Created by oden on 13/03/08.
+//  Copyright (c) 2013年 JITAKUKEIBI INC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OCppOperatorTests : SenTestCase
+
+@end
