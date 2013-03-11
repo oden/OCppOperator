24f23ee768dbd0a8efc3b7c104b05512b4af5197
oden <oden@jitakukeibi.in>
2013-03-11 15:15:59 +0900

    NSObject+OCppOperator.hにHeaderDoc追記 refs #8

--- OCppOperator/NSObject+OCppOperator.h
+++ OCppOperator/NSObject+OCppOperator.h
@@ -6,11 +6,24 @@
 //  Copyright (c) 2013年 JITAKUKEIBI INC. All rights reserved.
 //
 
+/*!
+ @header NSObject+OCppOperator.h
+ @abstract NSObjectにIDプロパティを付けるよ＼(^o^)／
+ */
 #import <Foundation/Foundation.h>
 #import "ID.h"
 
+/*!
+ @class NSObject
+ @abstract NSObjectのカテゴリ
+ */
 @interface NSObject(OCppOperator)
 
+/*!
+ @property ID
+ @abstract ID型のキャスト用プロパティ
+ @return selfをそのままreturnします。その時、ID型のコピーコンストラクタが実行されて、ID型への暗黙の型変換が発生します。
+ */
 @property (nonatomic, readonly) ID ID;
 
 @end
